91d8c6a272f4a69296326067c61dfc125a28529b
benjamin martin <bemartin@et.esiea.fr>
2022-10-18 22:36:16 +0200

    Create Launcher and edit UmlGraph for variadic parameters (part 2)

---
+++ src/main/java/fr/lernejo/umlgrapher/Launcher.java
@@ -0,0 +1,27 @@
+package fr.lernejo.umlgrapher;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import java.lang.Runnable;
+
+public class Launcher implements Runnable {
+    @Option(names = {"-c", "--classes"}, required = true, description = "Renseignemant de class")
+    private final Class[] user_class = null;
+    @Option(names = {"-g", "--graph-type"}, description = "Selected type of output")
+    private final GraphType graphType = GraphType.Mermaid;
+
+    @Override
+    public void run() {
+        UmlGraph graph = new UmlGraph(user_class);
+        String output = graph.as(graphType);
+        System.out.println(output);
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Launcher()).execute(args);
+        System.exit(exitCode);
+
+    }
+
+
+}
